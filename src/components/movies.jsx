7f8781d9b3d16b3a555a6fff340b56da866b393f
BFX09 <bardia.azari@gmail.com>
2022-05-14 15:42:27 +0430

    master

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,6 +5,7 @@ import MoviesTable from "./moviesTable";
 import ListGroup from "./common/listGroup";
 import Pagination from "./common/pagination";
 import SearchBox from "./searchBox";
+import LoadingSpinner from "./common/loadingSpinner";
 import { getMovies, deleteMovie } from "../services/movieService";
 import { getGenres } from "../services/genreService";
 import { paginate } from "../utils/paginate";
@@ -20,14 +21,21 @@ class Movies extends Component {
     searchQuery: "",
     selectedGenre: null,
     sortColumn: { path: "title", order: "asc" },
+    isLoading: false,
   };
 
   async componentDidMount() {
+    this.setState({ isLoading: true });
     const { data } = await getGenres();
     const genres = [{ _id: "", name: "All Genres" }, ...data];
 
     const { data: movies } = await getMovies();
-    this.setState({ movies, genres, selectedGenre: genres[0] });
+    this.setState({
+      movies,
+      genres,
+      selectedGenre: genres[0],
+      isLoading: false,
+    });
   }
 
   handleDelete = async (movie) => {
@@ -109,46 +117,53 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
+    const { pageSize, currentPage, sortColumn, searchQuery, isLoading } =
+      this.state;
     const { user } = this.props;
 
     const { totalCount, data: movies } = this.getPagedData();
 
     return (
       <div className="row">
-        <div className="col-3">
-          <ListGroup
-            items={this.state.genres}
-            selectedItem={this.state.selectedGenre}
-            onItemSelect={this.handleGenreSelect}
-          />
-        </div>
-        <div className="col">
-          {user && (
-            <Link
-              to="/movies/new"
-              className="btn btn-primary"
-              style={{ marginBottom: 20 }}
-            >
-              New Movie
-            </Link>
-          )}
-          <p>Showing {totalCount} movies in the database.</p>
-          <SearchBox value={searchQuery} onChange={this.handleSearch} />
-          <MoviesTable
-            movies={movies}
-            sortColumn={sortColumn}
-            onLike={this.handleLike}
-            onDelete={this.handleDelete}
-            onSort={this.handleSort}
-          />
-          <Pagination
-            itemsCount={totalCount}
-            pageSize={pageSize}
-            currentPage={currentPage}
-            onPageChange={this.handlePageChange}
-          />
-        </div>
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <div>
+            <div className="col-3">
+              <ListGroup
+                items={this.state.genres}
+                selectedItem={this.state.selectedGenre}
+                onItemSelect={this.handleGenreSelect}
+              />
+            </div>
+            <div className="col">
+              {user && (
+                <Link
+                  to="/movies/new"
+                  className="btn btn-primary"
+                  style={{ marginBottom: 20 }}
+                >
+                  New Movie
+                </Link>
+              )}
+              <p>Showing {totalCount} movies in the database.</p>
+              <SearchBox value={searchQuery} onChange={this.handleSearch} />
+              <MoviesTable
+                movies={movies}
+                sortColumn={sortColumn}
+                onLike={this.handleLike}
+                onDelete={this.handleDelete}
+                onSort={this.handleSort}
+              />
+              <Pagination
+                itemsCount={totalCount}
+                pageSize={pageSize}
+                currentPage={currentPage}
+                onPageChange={this.handlePageChange}
+              />
+            </div>
+          </div>
+        )}
       </div>
     );
   }
