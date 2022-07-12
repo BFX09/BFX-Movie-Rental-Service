d3ccd44c1217f4b723912293422fe565a152373f
BFX09 <bardia.azari@gmail.com>
2022-07-12 12:27:44 +0430

    Responsive Design.

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "bootstrap/js/src/dropdown";
 
 const ListGroup = ({
   items,
@@ -7,20 +8,66 @@ const ListGroup = ({
   selectedItem,
   onItemSelect,
 }) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const detectWindowWidth = () => setWindowWidth(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", detectWindowWidth);
+
+    return () => {
+      window.addEventListener("resize", detectWindowWidth);
+    };
+  }, [windowWidth]);
+
+  if (windowWidth > 740) {
+    return (
+      <ul className="list-group">
+        {items.map((item) => (
+          <li
+            onClick={() => onItemSelect(item)}
+            key={item[valueProperty]}
+            className={
+              item === selectedItem
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+          >
+            {item[textProperty]}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
   return (
-    <ul className="list-group">
-      {items.map((item) => (
-        <li
-          onClick={() => onItemSelect(item)}
-          key={item[valueProperty]}
-          className={
-            item === selectedItem ? "list-group-item active" : "list-group-item"
-          }
-        >
-          {item[textProperty]}
-        </li>
-      ))}
-    </ul>
+    <div className="dropdown">
+      <button
+        class="btn btn-primary dropdown-toggle"
+        type="button"
+        id="dropdownMenuButton"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
+        Filters
+      </button>
+      <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        {items.map((item) => (
+          <button
+            onClick={() => onItemSelect(item)}
+            key={item[valueProperty]}
+            className={
+              item === selectedItem
+                ? "dropdown-item active"
+                : "dropdown-item"
+            }
+          >
+            {item[textProperty]}
+          </button>
+        ))}
+      </div>
+    </div>
   );
 };
 
